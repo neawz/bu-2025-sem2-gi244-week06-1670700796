8a9f48aaabeeba5e3773b62c9ffff8589740bd9b
neawz <chayapat2549@gmail.com>
2026-02-20 09:50:19 +0700

    Exam-03 Complete

--- Assets/Scripts/Exam/PlayerControllerExam03.cs
+++ Assets/Scripts/Exam/PlayerControllerExam03.cs
@@ -9,6 +9,7 @@ public class PlayerControllerExam03 : MonoBehaviour
 
     public bool enableAutoFireMode;
     public float autoFireInterval = 0.1f;
+    public float nextFireTime = 0f;
 
     private float horizontalInput;
     private InputAction moveAction;
@@ -18,6 +19,7 @@ public class PlayerControllerExam03 : MonoBehaviour
     {
         moveAction = InputSystem.actions.FindAction("Move");
         shootAction = InputSystem.actions.FindAction("Shoot");
+        nextFireTime = Time.time;
     }
 
     // Update is called once per frame
@@ -35,9 +37,29 @@ public class PlayerControllerExam03 : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
-        if (shootAction.triggered)
+        if (shootAction.triggered && !enableAutoFireMode)
         {
             Instantiate(projectilePrefab, transform.position, transform.rotation);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (enableAutoFireMode)
+            {
+                enableAutoFireMode = false;
+            }
+            else
+            {
+                enableAutoFireMode = true;
+            }
+        }
+
+
+
+        if (enableAutoFireMode && Time.time >= nextFireTime)
+        {
+            Instantiate(projectilePrefab, transform.position, transform.rotation);
+            nextFireTime = Time.time + autoFireInterval;
         }
     }
 }
