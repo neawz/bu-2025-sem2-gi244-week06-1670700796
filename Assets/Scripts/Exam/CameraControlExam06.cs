8a57c34e476795be9a1aa6525011c5b4b42795ac
neawz <chayapat2549@gmail.com>
2026-02-20 10:55:13 +0700

    Exam-06 Incomplete

--- Assets/Scripts/Exam/CameraControlExam06.cs
+++ Assets/Scripts/Exam/CameraControlExam06.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraControlExam06 : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class CameraControlExam06 : MonoBehaviour
     public float offset;
     public Camera targetCamera;
 
+    public float width;
+    public float height;
+
     // Update is called once per frame
     void LateUpdate()
     {
@@ -14,5 +18,11 @@ public class CameraControlExam06 : MonoBehaviour
         Vector3 player2Pos = player2.transform.position;
 
         // Student code ...
+        width = (Mathf.Abs(player1Pos.x - player2Pos.x) + offset) / targetCamera.aspect;
+        height = Mathf.Abs(player2Pos.z - player1Pos.z) + offset;
+        
+        targetCamera.orthographicSize = Mathf.Max(width, height);
+
+        
     }
 }
