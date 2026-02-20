b36ef5d86106de1803e033945df1c0f64a359d95
neawz <chayapat2549@gmail.com>
2026-02-20 10:17:00 +0700

    Exam-05 Complete

--- Assets/Scripts/Exam/PlayerControllerExam05.cs
+++ Assets/Scripts/Exam/PlayerControllerExam05.cs
@@ -11,6 +11,9 @@ public class PlayerControllerExam05 : MonoBehaviour
     // Exam 05 ...
     public int maxBulletCount = 10;
     public float bulletRegenerateCooldown = 1f;
+
+    public float nextReload;
+    public float remainBullet;
     // ...
 
     private float horizontalInput;
@@ -21,6 +24,12 @@ public class PlayerControllerExam05 : MonoBehaviour
     {
         moveAction = InputSystem.actions.FindAction("Move");
         shootAction = InputSystem.actions.FindAction("Shoot");
+    }
+
+    private void Start()
+    {
+        nextReload = Time.time;
+        remainBullet = maxBulletCount;
     }
 
     // Update is called once per frame
@@ -38,9 +47,18 @@ public class PlayerControllerExam05 : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
-        if (shootAction.triggered)
+        if (shootAction.triggered && Time.time >= nextReload)
         {
             Instantiate(projectilePrefab, transform.position, transform.rotation);
+            remainBullet--;
+            Debug.Log($"Bullet: {remainBullet}");
+        }
+
+        if (remainBullet == 0)
+        {
+            Debug.Log("Reload");
+            nextReload = Time.time + bulletRegenerateCooldown;
+            remainBullet = maxBulletCount;
         }
     }
 }
