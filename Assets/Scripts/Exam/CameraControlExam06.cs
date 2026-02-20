2a761364fd54cf3e222344d0e59e6a72b09b2d49
neawz <chayapat2549@gmail.com>
2026-02-20 11:26:20 +0700

    Finish touch

--- Assets/Scripts/Exam/CameraControlExam06.cs
+++ Assets/Scripts/Exam/CameraControlExam06.cs
@@ -20,7 +20,7 @@ public class CameraControlExam06 : MonoBehaviour
         // Student code ...
         width = (Mathf.Abs(player1Pos.x - player2Pos.x) / targetCamera.aspect) + offset;
         height = Mathf.Abs(player2Pos.z - player1Pos.z) + offset;
-        targetCamera.transform.position = new Vector3(player1Pos.x, 40, player2Pos.z);
+        targetCamera.transform.position = new Vector3((player1Pos.x + player2Pos.x)/2, 40, (player2Pos.z + player1Pos.z)/2);
         targetCamera.orthographicSize = Mathf.Max(width, height);
 
         
